891f0fddb5cb1c62ff830358e6080367ac46e01c
Kazeem Hakeem <hakymulla@gmail.com>
2020-03-13 09:12:44 +0100

    Initial commit

---
+++ urlshort.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
+import os.path
+
+app = Flask(__name__)
+app.secret_key = '123'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return '<h1> This is a URL Shortener </h1>'
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {}
+        if os.path.exists('urls.json'):
+            with open('urls.json') as url_file:
+                urls = json.load(url_file)
+        if request.form['code'] in urls.keys():
+            flash('Message exist')
+            return redirect(url_for('index'))
+
+        urls[request.form['code']] = {'url':request.form['url']}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+        return render_template('your_url.html', code=request.form['code'])
+    else:
+        return redirect(url_for('index'))
+
