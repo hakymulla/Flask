4effb8e03266e435ec276a7791a4e98d279a6dc7
Kazeem Hakeem <hakymulla@gmail.com>
2020-03-13 11:28:34 +0100

    added error handler

--- urlshort.py
+++ urlshort.py
@@ -1,17 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, abort, session
 import json
 import os.path
 
 app = Flask(__name__)
 app.secret_key = '123'
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return '<h1> This is a URL Shortener </h1>'
+
 
 @app.route('/your-url', methods=['GET', 'POST'])
 def your_url():
@@ -24,12 +27,14 @@ def your_url():
             flash('Message exist')
             return redirect(url_for('index'))
 
-        urls[request.form['code']] = {'url':request.form['url']}
+        urls[request.form['code']] = {'url': request.form['url']}
         with open('urls.json', 'w') as url_file:
             json.dump(urls, url_file)
+            session[request.form['code']] = True
         return render_template('your_url.html', code=request.form['code'])
     else:
         return redirect(url_for('index'))
+
 
 @app.route('/<string:code>')
 def redirect_to_url(code):
@@ -40,3 +45,9 @@ def redirect_to_url(code):
                 if 'url' in urls[code].keys():
                     return redirect(urls[code]['url'])
 
+    return abort(404)
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template('page_not_found.html'), 404
